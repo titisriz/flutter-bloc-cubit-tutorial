472f6d2941d21c8a4d8ed7c13c48346bb7ede373
titis <titis.rizki@gmail.com>
2022-03-12 20:08:48 +0700

    implement cubit

--- lib/pages/weather_search_page.dart
+++ lib/pages/weather_search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
 import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
 
 class WeatherSearchPage extends StatefulWidget {
@@ -17,7 +19,25 @@ class _WeatherSearchPageState extends State<WeatherSearchPage> {
         padding: EdgeInsets.symmetric(vertical: 16),
         alignment: Alignment.center,
         // TODO: Implement with cubit
-        child: buildInitialInput(),
+        child: BlocConsumer<WeatherCubit, WeatherState>(
+          listener: (context, state) {
+            if (state is WeatherError) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(state.message)));
+            }
+          },
+          builder: (context, state) {
+            if (state is WeatherInitial) {
+              return buildInitialInput();
+            } else if (state is WeatherLoading) {
+              return buildLoading();
+            } else if (state is WeatherLoaded) {
+              return buildColumnWithData(state.weather);
+            } else {
+              return buildInitialInput();
+            }
+          },
+        ),
       ),
     );
   }
@@ -75,5 +95,7 @@ class CityInputField extends StatelessWidget {
 
   void submitCityName(BuildContext context, String cityName) {
     // TODO: Get weather for the city
+    final weatherCubit = context.read<WeatherCubit>();
+    weatherCubit.getWeather(cityName);
   }
 }
